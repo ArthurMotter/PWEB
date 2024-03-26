a8d883a487e97b09d02d080ea07a82ffc0b68a66
arthur motter <arthur.motter.fatec@gmail.com>
2024-03-26 15:00:19 -0300

    operacoes ativ 6

--- atividade6/Operacoes.js
+++ atividade6/Operacoes.js
@@ -1,11 +1,21 @@
 //arquivo javascript para operacoes
 
+var num1, num2;
+
+num1 = parseFloat(prompt("digite o primeiro numero: "));
+num2 = parseFloat(prompt("digite o segundo numero: "));
+
 //soma
+alert("Soma dos numeros: "+ (num1 + num2) );
 
 //subtracao do 1o pelo 2o
+alert("Subtracao dos numeros: "+ (num1 - num2) );
 
 //produto dos dois
+alert("Produto dos numeros: "+ (num1 * num2) );
 
 //divisao do 1o pelo 2o
+alert("Divisao dos numeros: "+ (num1 / num2) );
 
 //resto da divisao do 1o pelo 2o
+alert("Resto da divisao dos numeros: "+ (num1 % num2) );
