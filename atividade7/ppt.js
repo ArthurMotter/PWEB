123207310600915886bfb371fc1d2ac053e2d368
ARTHUR JOSE FERRO MOTTER <0030482221013@lti.fatecso.edu.br>
2024-04-01 11:56:36 -0300

    atividade 7 fim

--- atividade7/ppt.js
+++ atividade7/ppt.js
@@ -1,42 +1,44 @@
-var escolhaH, escolhaBot;
-escolhaH = prompt("digite sua escolha: ");
+var escolhaH, escolhaBot, t=1;
+do{
+    escolhaH = prompt("digite sua escolha: ");
 
-//fazendo bot escolher
-escolhaBot = Math.random();
-
-if(escolhaBot < 0.33){
-    escolhaBot = "pedra";
-}
-else if(escolhaBot < 0.66){
-    escolhaBot = "papel";
-}
-else{
-    escolhaBot = "tesoura";
-}
-
-alert("escolha do bot: " + escolhaBot);
-
-//jogador humano ganha
-if(escolhaBot == "pedra" && escolhaH == "papel"){
-    alert("humano venceu");
-}
-else if(escolhaBot == "papel" && escolhaH == "tesoura"){
-    alert("humano venceu");
-}
-else if(escolhaBot == "tesoura" && escolhaH == "pedra"){
-    alert("humano venceu");
-}
-//jogador bot ganha
-else if(escolhaH == "pedra" && escolhaBot == "papel"){
-    alert("robo venceu");
-}
-else if(escolhaH == "papel" && escolhaBot == "tesoura"){
-    alert("robo venceu");
-}
-else if(escolhaH == "tesoura" && escolhaBot == "pedra"){
-    alert("robo venceu");
-}
-//jogador nenhum ganha
-else{
-    alert("ninguem venceu, empate");
-}
+    //fazendo bot escolher
+    escolhaBot = Math.random();
+    
+    if(escolhaBot < 0.33){
+        escolhaBot = "pedra";
+    }
+    else if(escolhaBot < 0.66){
+        escolhaBot = "papel";
+    }
+    else{
+        escolhaBot = "tesoura";
+    }
+    
+    alert("escolha do bot: " + escolhaBot);
+    
+    //jogador humano ganha
+    if(escolhaBot == "pedra" && escolhaH == "papel"){
+        alert("humano venceu");
+    }
+    else if(escolhaBot == "papel" && escolhaH == "tesoura"){
+        alert("humano venceu");
+    }
+    else if(escolhaBot == "tesoura" && escolhaH == "pedra"){
+        alert("humano venceu");
+    }
+    //jogador bot ganha
+    else if(escolhaH == "pedra" && escolhaBot == "papel"){
+        alert("robo venceu");
+    }
+    else if(escolhaH == "papel" && escolhaBot == "tesoura"){
+        alert("robo venceu");
+    }
+    else if(escolhaH == "tesoura" && escolhaBot == "pedra"){
+        alert("robo venceu");
+    }
+    //jogador nenhum ganha
+    else{
+        alert("ninguem venceu, empate");
+    }
+} while(t!=0)
