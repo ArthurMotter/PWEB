f2b7603330e5210181aefdde33f86d2b9f736ef8
ARTHUR JOSE FERRO MOTTER <0030482221013@lti.fatecso.edu.br>
2024-04-01 11:35:04 -0300

    pedra papel tesoura

---
+++ atividade7/ppt.js
@@ -0,0 +1,42 @@
+var escolhaH, escolhaBot;
+escolhaH = prompt("digite sua escolha: ");
+
+//fazendo bot escolher
+escolhaBot = Math.random();
+
+if(escolhaBot < 0.33){
+    escolhaBot = "pedra";
+}
+else if(escolhaBot < 0.66){
+    escolhaBot = "papel";
+}
+else{
+    escolhaBot = "tesoura";
+}
+
+alert("escolha do bot: " + escolhaBot);
+
+//jogador humano ganha
+if(escolhaBot == "pedra" && escolhaH == "papel"){
+    alert("humano venceu");
+}
+else if(escolhaBot == "papel" && escolhaH == "tesoura"){
+    alert("humano venceu");
+}
+else if(escolhaBot == "tesoura" && escolhaH == "pedra"){
+    alert("humano venceu");
+}
+//jogador bot ganha
+else if(escolhaH == "pedra" && escolhaBot == "papel"){
+    alert("robo venceu");
+}
+else if(escolhaH == "papel" && escolhaBot == "tesoura"){
+    alert("robo venceu");
+}
+else if(escolhaH == "tesoura" && escolhaBot == "pedra"){
+    alert("robo venceu");
+}
+//jogador nenhum ganha
+else{
+    alert("ninguem venceu, empate");
+}
