49aa9b0dad3f470642a66e4baafbb8ecd078af2c
arthur motter <arthur.motter.fatec@gmail.com>
2024-05-27 16:16:12 -0300

    node tests

--- ProjetoFinal/tests/app/server.js
+++ ProjetoFinal/tests/app/server.js
@@ -10,6 +10,25 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'pages/index.html'));
 });
 
+// ... (server.js)
+
+// Route for a "menu" page
+app.get('/menu', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/menu.html'));
+});
+// Route for a "edit" page
+app.get('/menu', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/edit_file.html'));
+});
+// Route for a "create" page
+app.get('/menu', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/create_album.html'));
+});
+// Route for a "view" page
+app.get('/menu', (req, res) => {
+  res.sendFile(path.join(__dirname, 'pages/view_file.html'));
+});
+
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
