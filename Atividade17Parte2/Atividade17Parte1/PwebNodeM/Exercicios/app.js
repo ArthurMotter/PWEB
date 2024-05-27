489d1d89ddf9f2eb8aa62b9c14c4b8c906d03598
ARTHUR JOSE FERRO MOTTER <0030482221013@lti.fatecso.edu.br>
2024-05-27 10:47:23 -0300

    app fix

--- Atividade17Parte2/Atividade17Parte1/PwebNodeM/Exercicios/app.js
+++ Atividade17Parte2/Atividade17Parte1/PwebNodeM/Exercicios/app.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var app = express();
+ 
+app.set('view engene','ejs')
+ 
+app.get('',function(req,res){
+    res.send("<html><body>Site da Fatec Sorocaba</body></html>");
+});
 app.get('/historia',function(req,res){
-    res.send("<html><body>História da Fatec Sorocaba</body></html>");
+    res.render("secao/historia");
 });
 app.get('/cursos',function(req,res){
-    res.send("<html><body>Cursos da Fatec Sorocaba</body></html>");
+    res.render("secao/cursos");
 });
 app.get('/professores',function(req,res){
-    res.send("<html><body>Professores da Fatec Sorocaba</body></html>");
+    res.render("secao/professores");
 });
 app.listen(3000,function(){
     console.log("servidor com express foi carregado");
