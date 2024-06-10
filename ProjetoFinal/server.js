f9f91fa3056ddfc8a9cf6151cc3a41bafd1ddbf6
arthur motter <arthur.motter.fatec@gmail.com>
2024-06-10 19:42:55 -0300

    attempt to link render +9

--- ProjetoFinal/server.js
+++ ProjetoFinal/server.js
@@ -24,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public', 'data', 'uploads'))); // T
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Define routes for your pages
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'pages', 'menu.html')); 
 });
