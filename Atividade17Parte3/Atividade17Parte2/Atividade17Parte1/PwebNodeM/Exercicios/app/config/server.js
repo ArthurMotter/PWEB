dc2b8e705aac29beab917e6d201d7bf8d08548d1
ARTHUR JOSE FERRO MOTTER <0030482221013@lti.fatecso.edu.br>
2024-06-10 09:59:15 -0300

    part 4

--- Atividade17Parte3/Atividade17Parte2/Atividade17Parte1/PwebNodeM/Exercicios/app/config/server.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
- 
-app.set('view engine','ejs') //mecanismo engine
-app.set('views','./app/views'); //diretorio
-
-module.exports = app;
--- Atividade17Parte3/Atividade17Parte2/Atividade17Parte1/PwebNodeM/Exercicios/app/routes/historia.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app){
-    app.get('/historia',function(req,res){
-        res.render("informacao/historia");
-    });
-}
